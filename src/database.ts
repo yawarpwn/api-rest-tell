b1b9b0842edc075ce2fdb9aab4ca4798c7e28cfc
yawarpwn <yawarpwn@gmail.com>
2021-08-19 14:13:27 -0500

    db revelado

--- src/database.ts
+++ src/database.ts
@@ -9,7 +9,7 @@ import config from './config'
 
 			//user: config.MONGO_USER, 
 		}
-		const db = await mongoose.connect(config.MONGODB_URI, mongooseOptions)
+		const db = await mongoose.connect(`mongodb+srv://yawarpwn:Db4524987@cluster0.ms9fs.mongodb.net/tellDatabase?retryWrites=true&w=majority`, mongooseOptions)
 		console.log('database is connect to:', db.connection.name)
 	}	catch(err) {
 		console.error(err)
