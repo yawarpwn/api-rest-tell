c6ea6414ac2617fb0f19255668e2fba038d16970
yawarpwn <yawarpwn@gmail.com>
2021-08-18 23:59:47 -0500

    procfile agregado

--- src/database.ts
+++ src/database.ts
@@ -10,7 +10,7 @@ import config from './config'
 			//user: config.MONGO_USER, 
 		}
 		const db = await mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_HOST}/${config.MONGO_DATABASE}?retryWrites=true&w=majority`, mongooseOptions)
-		console.log('database is connect to:', db.connection.name, config.MONGO_HOST)
+		console.log('database is connect to:', db.connection.name)
 	}	catch(err) {
 		console.error(err)
 	}
